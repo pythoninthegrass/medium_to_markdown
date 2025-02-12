c67524d6ff4d6aa21a6de4074eb167999cd667bf
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-02-11 18:01:21 -0600

    refactor: multiprocessing

--- README.md
+++ README.md
@@ -7,8 +7,21 @@
 Two for loops, one to iterate over HTML files and another to append to the bookmarks markdown file.
 
 ```bash
+# sync
 λ hyperfine ./main.py --warmup 1
 Benchmark 1: ./main.py
   Time (mean ± σ):     154.7 ms ±   3.1 ms    [User: 146.3 ms, System: 6.7 ms]
   Range (min … max):   151.1 ms … 162.6 ms    19 runs
+
+# async
+λ hyperfine ./main.py --warmup 1
+Benchmark 1: ./main.py
+  Time (mean ± σ):     172.2 ms ±   3.0 ms    [User: 159.1 ms, System: 11.7 ms]
+  Range (min … max):   168.6 ms … 179.3 ms    17 runs
+
+# multiprocessing
+λ hyperfine ./main.py --warmup 1
+Benchmark 1: ./main.py
+  Time (mean ± σ):     157.0 ms ±   4.9 ms    [User: 994.9 ms, System: 186.5 ms]
+  Range (min … max):   149.1 ms … 168.1 ms    19 runs
 ```
--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
 
-import aiofiles
-import asyncio
 from markdownify import markdownify
+from multiprocessing import Pool
 from pathlib import Path
 
-async def process_file(file_path):
-    async with aiofiles.open(file_path, "r") as f:
-        html_file = await f.read()
+def process_file(file_path):
+    with open(file_path, "r") as f:
+        html_file = f.read()
     md_text = markdownify(html_file)
     return "##" + md_text.split("###")[1]
 
-async def write_results(fp, md_text_list):
-    async with aiofiles.open(fp, "w") as f:
-        for i, md_text in enumerate(md_text_list):
-            await f.write(md_text)
-            if i < len(md_text_list) - 1:
-                await f.write("\n")
-
-async def main():
+def main():
     file_list = sorted(Path('./data/bookmarks').glob('*.html'))
 
-    # Process all files in parallel
-    tasks = [process_file(file_path) for file_path in file_list]
-    md_text_list = await asyncio.gather(*tasks)
+    with Pool() as pool:
+        md_text_list = pool.map(process_file, file_list)
 
-    # Write results
     fp = Path("data/bookmarks.md").resolve()
     fp.parent.mkdir(exist_ok=True)
     fp.touch()
 
-    await write_results(fp, md_text_list)
+    with open(fp, "w") as f:
+        f.write("\n".join(md_text_list))
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
