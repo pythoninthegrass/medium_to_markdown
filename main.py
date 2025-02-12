c152a409ad490369bca3c0017ea65a9c7f6046f8
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-02-11 18:02:41 -0600

    fix: revert to original sync setup

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 
 from markdownify import markdownify
-from multiprocessing import Pool
 from pathlib import Path
 
-def process_file(file_path):
-    with open(file_path, "r") as f:
-        html_file = f.read()
+file_list = sorted(Path('./data/bookmarks').glob('*.html'))
+
+md_text_list = []
+
+for file_path in file_list:
+    html_file = open(file_path, "r").read()
     md_text = markdownify(html_file)
-    return "##" + md_text.split("###")[1]
+    md_text_list.append("##" + md_text.split("###")[1])
 
-def main():
-    file_list = sorted(Path('./data/bookmarks').glob('*.html'))
+fp = Path("data/bookmarks.md").resolve()
+fp.parent.mkdir(exist_ok=True)
+fp.touch()
 
-    with Pool() as pool:
-        md_text_list = pool.map(process_file, file_list)
-
-    fp = Path("data/bookmarks.md").resolve()
-    fp.parent.mkdir(exist_ok=True)
-    fp.touch()
-
-    with open(fp, "w") as f:
-        f.write("\n".join(md_text_list))
-
-if __name__ == "__main__":
-    main()
+with open(fp, "w") as f:
+    for i, md_text in enumerate(md_text_list):
+        f.write(md_text)
+        if i < len(md_text_list) - 1:
+            f.write("\n")
